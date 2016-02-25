bdef497f4f0bde1361e50e44c0624f544f38ecf5
iknow0614 <xiongwei_13@hotmail.com>
2016-02-25 16:14:59 +0800

    完成试验版本

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # zupload
+
+为系统平台提供统一的文件管理模块，包括文件上传、文件删除、文件下载。
+
+## API
+
+接口 | 方法 | 参数 | 说明
+---|---|---|---
+/up | POST | input | 向 up 发起 post 请求，文件存入 ./upload_dir/yyyymmdd/ 目录，成功后返回详细信息(json)
+/del | GET | path | 向 del 发起 get 请求，参数 path 为文件路径
+/down | GET | path,name | 向 down 发起 get 请求，path 为文件路径，name 为文件名(name 可选)
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ var fs = require('fs');
 
 var formidable = require('formidable');
 
+// 上传目录
+var dir = path.join(__dirname, '../upload_dir');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,10 +19,10 @@ router.get('/', function(req, res, next) {
 // 文件上传
 router.post('/up', function(req, res, next) {
   // 上传文件夹
-  var uploadDir = path.join(__dirname, '../upload_dir', moment().format('YYYYMMDD'));
+  var uploadDir = path.join(dir, moment().format('YYYYMMDD'));
 
   // 查看是否存在上传文件夹
-  if(!fs.exists(uploadDir)) {
+  if(!fs.existsSync(uploadDir)) {
     fs.mkdir(uploadDir, function(error) {
       console.log(error);
     });
@@ -33,8 +35,39 @@ router.post('/up', function(req, res, next) {
   form.multiples = true;
 
   form.parse(req, function(err, fields, files) {
-    res.send(JSON.stringify(files));
+    var reg = new RegExp(dir, 'g');
+    var output = JSON.stringify(files).replace(reg, '');
+    res.send(output);
   });
 });
+
+
+// 文件删除
+router.get('/del', function(req, res, next) {
+  var filePath = req.query.path;
+  delFile = path.join(dir, filePath);
+  fs.unlink(delFile, function(err) {
+    if(err) {
+      console.log(err);
+      return res.send({res: 'not found'});
+    }
+    res.send({res: 'ok'});
+  });
+});
+
+
+// 文件下载
+router.get('/down', function(req, res, next) {
+  if(!req.query.path) return res.send({res: 'no path'});
+  var filePath = path.join(dir, req.query.path);
+  var fileName = req.query.name;
+  res.download(filePath, fileName, function(err) {
+    if(err) {
+      console.log(err);
+      return res.send({res: 'not found'});
+    }
+  });
+});
+
 
 module.exports = router;
